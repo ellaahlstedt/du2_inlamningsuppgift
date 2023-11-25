2c74f08c405ddf04cd2a979da003bb5806793401
Ella <ellaahl02@gmail.com>
2023-11-25 23:03:16 +0100

    Files added

---
+++ components/filters/subject_filters/subject_filters.js
@@ -0,0 +1,16 @@
+
+function render_subject_filters(parent)
+{
+  // Funktionen tar emot en referens till förälderelementet.
+  // Den skapar en container för alla filter-element för "subject".
+  // Se language_filters.js  
+
+  const container = document.createElement("div");
+  container.id = "subject_filters";
+  parent.append(container);
+  
+  for (let subject of SUBJECTS)
+  {
+    render_filter_element(container, subject.name);
+  }
+}
---
+++ components/header/header.js
@@ -0,0 +1,46 @@
+
+
+function render_header(parent)
+{
+  // Funktionen tar emot referensen till container-noden för headern.
+  // Funktionsanropet finns redan på plats i index.js. Studera argumentet som skickas där.
+  // Funktionen skapar de nödvändiga HTML-elementen så att headern ser ut
+  // som den på videon: Tre random bilder och texten centrerad.
+
+  // Krav:
+  // Nyckeln *style* får i denna kod endast användas för att ange bakgrundsbild-filen.
+  // Det går dock såklart även att lösa det hela med <img> istället för att ha bilderna
+  // som bakgrundsbilder. I så fall får inte nyckeln *style* användas alls i koden här.
+
+  // Glöm inte: Du måste även fylla i header.css så att headern ser ut som den på videon. Se header.css
+  
+  let headerCon = document.createElement("div");
+  headerCon.classList.add("headerContainer");
+  parent.appendChild(headerCon);
+
+  let imageCon = document.createElement("div");
+  imageCon.classList.add("headerImageContainer");
+  headerCon.appendChild(imageCon);
+
+  // lägg till h1 :)))
+  
+
+  // countries[ random].imagesNormal[random]
+  for (let i = 0; i < 3; i++) {
+    let image = document.createElement("div");
+    image.classList.add(`image${i}`);
+    let randomCountry = Math.floor(Math.random()*COUNTRIES.length)
+    let randomImage = Math.floor(Math.random()*COUNTRIES[randomCountry].imagesNormal.length)
+    
+    // console.log(`(url: ./media/geo_images/${COUNTRIES[randomCountry].imagesNormal[randomImage]})`)
+    // image.style.backgroundColor = "red";
+    image.style.backgroundImage = `url(./media/geo_images/${COUNTRIES[randomCountry].imagesNormal[randomImage]})`;
+    // image.setAttribute("src", )
+    imageCon.appendChild(image);
+  }
+
+  let image2 = document.querySelector(".image1");
+  let h1 = document.createElement("h1");
+  h1.innerHTML = `Utbytesstudier <br> Missa inte chansen!`;
+  image2.appendChild(h1);
+}
